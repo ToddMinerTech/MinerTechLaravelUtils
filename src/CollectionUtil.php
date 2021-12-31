cfb191da4701f826f6562a34f0e2c2d754e4574b
Todd Miner <todd@miner.technology>
2021-12-31 15:02:50 -0800

    First commit for new clone of ArrUtil

--- src/CollectionUtil.php
+++ src/CollectionUtil.php
@@ -2,82 +2,86 @@
 
 declare(strict_types=1);
 
-namespace ToddMinerTech\DataUtils;
+namespace ToddMinerTech\LaravelUtils;
 
+use Illuminate\Support\Collection;
 use ToddMinerTech\DataUtils\StringUtil;
 
 /**
- * Class ArrUtil
+ * Class CollectionUtil
  *
- * Various array editing, search, and comparison functions
+ * Various collection editing, search, and comparison functions
  *
  * @package ToddMinerTech\MinerTechLaravelUtils
  */
-class ArrUtil
+class CollectionUtil
 {
     /**
-     * addArrIfNew
+     * addCollectionIfNew
      *
-     * Inserts a value into an array only if it doesn't exist yet
+     * Inserts a value into an collection only if it doesn't exist yet
      * 
-     * @param string $inputStr New value to insert into array
+     * @param string $inputStr New value to insert into collection
+     * 
+     * @param ByRef Collection $inputCollection Collection to search
      *
-     * @return array Returns the array with any update needed
+     * @return bool Returns true if the new item was inserted, false if not
      */
-    public static function addArrIfNew(string $newValue, array $inputArr): array
+    public static function addCollectionIfNew(string $newValue, Collection &$inputCollection): bool
     {
-        if(!in_array($newValue, $inputArr)) {
-            $inputArr[] = $newValue;
+        if(!$inputCollection->contains($newValue)) {
+            $inputCollection->push($newValue);
+            return true;
         }
-        return $inputArr;
+        return false;
     }
     
     /**
-     * matchValueInObjectArray
+     * matchValueInObjectCollection
      *
-     * Searches and returns an object from within an array using defined criteria.
+     * Searches and returns an object from within an collection using defined criteria.
      * 
      * @param string $valueToMatch The value you are trying to match within the object attribute
      * 
      * @param string $attributeToMatch The attribute name you want to check the value of
      * 
-     * @param array $arrayToSearch The array of objects you are trying to match
+     * @param collection $collectionToSearch The collection of objects you are trying to match
      *
      * @return object Returns the object if matched, or null if no match is found
      */
-    public static function matchValueInObjectArray(string $valueToMatch, string $attributeToMatch, array $arrayToSearch): ?object
+    public static function matchValueInObjectCollection(string $valueToMatch, string $attributeToMatch, Collection $collectionToSearch): ?object
     {
-        for($i = 0; $i < count($arrayToSearch); $i++) {
-            if(!isset($arrayToSearch[$i]->$attributeToMatch)) {
+        for($i = 0; $i < count($collectionToSearch); $i++) {
+            if(!isset($collectionToSearch[$i]->$attributeToMatch)) {
                 continue;
             }
-            if(StringUtil::sSComp($arrayToSearch[$i]->$attributeToMatch, $valueToMatch)) {
-                return $arrayToSearch[$i];
+            if(StringUtil::sSComp($collectionToSearch[$i]->$attributeToMatch, $valueToMatch)) {
+                return $collectionToSearch[$i];
             }
         }
         return null;
     }
     
     /**
-     * matchValueInObjectArrayIndex
+     * matchValueInObjectCollectionIndex
      *
-     * Same as matchValueInObjectArray but returns the index value instead.   
+     * Same as matchValueInObjectCollection but returns the index value instead.   
      * 
      * @param string $valueToMatch The value you are trying to match within the object attribute
      * 
      * @param string $attributeToMatch The attribute name you want to check the value of
      * 
-     * @param array $arrayToSearch The array of objects you are trying to match
+     * @param collection $collectionToSearch The collection of objects you are trying to match
      *
      * @return object Returns the index of the object, or -1 if no match
      */
-    public static function matchValueInObjectArrayIndex(string $valueToMatch, string $attributeToMatch, array $arrayToSearch): int|bool
+    public static function matchValueInObjectCollectionIndex(string $valueToMatch, string $attributeToMatch, Collection $collectionToSearch): int|bool
     {
-        for($i = 0; $i < count($arrayToSearch); $i++) {
-            if(!isset($arrayToSearch[$i]->$attributeToMatch)) {
+        for($i = 0; $i < count($collectionToSearch); $i++) {
+            if(!isset($collectionToSearch[$i]->$attributeToMatch)) {
                 continue;
             }
-            if(StringUtil::sSComp($arrayToSearch[$i]->$attributeToMatch, $valueToMatch)) {
+            if(StringUtil::sSComp($collectionToSearch[$i]->$attributeToMatch, $valueToMatch)) {
                 return $i;
             }
         }
@@ -85,30 +89,30 @@ class ArrUtil
     }
     
     /**
-     * matchValueInObjectArrayClosure
+     * matchValueInObjectCollectionClosure
      *
-     * Searches and returns an object from within an array using an anonymous function.
+     * Searches and returns an object from within an collection using an anonymous function.
      * 
      * @param string $matchCriteria An anonymous function that takes a comparable object returns bool value.  True if a record matches.
      * 
-     * @param array $arrayToSearch The array of objects you are trying to match
+     * @param collection $collectionToSearch The collection of objects you are trying to match
      *
      * @return object Returns the object if matched, or null if no match is found
      */
-    public static function matchValueInObjectArrayClosure(Closure $matchCriteria, array $arrayToSearch): ?object
+    public static function matchValueInObjectCollectionClosure(Closure $matchCriteria, Collection $collectionToSearch): ?object
     {
-        for($i = 0; $i < count($arrayToSearch); $i++) {
-            if($matchCriteria($arrayToSearch[$i])) {
-                return $arrayToSearch[$i];
+        for($i = 0; $i < count($collectionToSearch); $i++) {
+            if($matchCriteria($collectionToSearch[$i])) {
+                return $collectionToSearch[$i];
             }
         }
         return null;
     }
 
     /**
-     * mapValueInObjectArray
+     * mapValueInObjectCollection
      *
-     * Wraps matchValueInObjectArray to provide a specific mapped output value from search criteria and array of objects.
+     * Wraps matchValueInObjectCollection to provide a specific mapped output value from search criteria and collection of objects.
      * 
      * @param string $valueToMatch The value you are trying to match within the object attribute
      * 
@@ -116,13 +120,13 @@ class ArrUtil
      * 
      * @param string $attributeToReturn The attribute name you want to return from the matched object
      * 
-     * @param array $arrayToSearch The array of objects you are trying to match
+     * @param collection $collectionToSearch The collection of objects you are trying to match
      *
      * @return string Returns the mapped value if a match is found, otherwise returns the input value unchanged
      */
-    public static function mapValueInObjectArray(string $valueToMatch, string $attributeToMatch, string $attributeToReturn, array $arrayToSearch): string
+    public static function mapValueInObjectCollection(string $valueToMatch, string $attributeToMatch, string $attributeToReturn, Collection $collectionToSearch): string
     {
-        $matchedObject = self::matchValueInObjectArray($valueToMatch, $attributeToMatch, $arrayToSearch);
+        $matchedObject = self::matchValueInObjectCollection($valueToMatch, $attributeToMatch, $collectionToSearch);
         if(!$matchedObject || !isset($matchedObject->$attributeToReturn)) {
             return $valueToMatch;
         }
